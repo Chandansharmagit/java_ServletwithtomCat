2a65fe1be2184c7e068f433c98c997661cca6d69
chandan sharma <utamsharma57@gmail.com>
2024-07-05 18:37:48 +0545

    first commit

---
+++ src/main/java/org/example/new_servletchandanwithtomcat/newclass.java
@@ -0,0 +1,7 @@
+package org.example.new_servletchandanwithtomcat;
+
+public class newclass {
+    public static void main(String[] args) {
+        System.out.println("hello java class servlet");
+    }
+}
